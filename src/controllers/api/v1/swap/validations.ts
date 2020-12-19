f6810f48db85bc40987f2f11c7839c1af1552c6a
Mulenga Bowa <mulengabowa53@gmail.com>
2020-12-19 14:51:28 +0200

    add new validation isCurrencyOrAddress

--- src/controllers/api/v1/swap/validations.ts
+++ src/controllers/api/v1/swap/validations.ts
@@ -1,9 +1,10 @@
 import { body } from 'express-validator'
+import isCurrencyOrAddress from '../../../../utils/validators/isCurrencyOrAddress'
 import isAddress from '../../../../utils/validators/isAddress'
 
 export const swapCallParametersValidation = [
-  body('inputCurrency').exists().isString().custom(isAddress),
-  body('outputCurrency').exists().isString().custom(isAddress),
+  body('inputCurrency').exists().isString().custom(isCurrencyOrAddress),
+  body('outputCurrency').exists().isString().custom(isCurrencyOrAddress),
   body('inputAmount').exists().isString(),
   body('recipient').exists().isString().custom(isAddress),
 ]
