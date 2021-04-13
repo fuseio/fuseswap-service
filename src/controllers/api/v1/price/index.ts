1d865de17e34e677f878d84597e6b4947dfc0297
Lior Agnin <lior.agnin@gmail.com>
2021-04-13 17:06:48 +0300

    temp fix for getting fUSD price

--- src/controllers/api/v1/price/index.ts
+++ src/controllers/api/v1/price/index.ts
@@ -1,6 +1,11 @@
 import { Request, Response, NextFunction } from 'express'
 import Container from 'typedi'
 import TokenService from '@services/token'
+import get from 'lodash.get'
+
+const tokensMap = {
+  '0x249be57637d8b013ad64785404b24aebae9b098b': '0x620fd5fa44BE6af63715Ef4E65DDFA0387aD13F5'
+}
 
 export default {
   async getPrice (req: Request, res: Response, next: NextFunction) {
@@ -9,7 +14,7 @@ export default {
 
       const tokenService = Container.get(TokenService)
 
-      const price = await tokenService.getTokenPrice(tokenAddress)
+      const price = await tokenService.getTokenPrice(get(tokensMap, tokenAddress.toLowerCase(), tokenAddress))
 
       res.send({ data: { price } })
     } catch (e) {
