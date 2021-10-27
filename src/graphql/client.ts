34982430cd0d4534682baf7736ec4ac47bd76960
mul53 <mulengabowa53@gmail.com>
2021-10-27 15:38:55 +0200

    change subgraph links

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from 'graphql-request'
 
-export const fuseswapClient = new GraphQLClient('https://graph.fuse.io/subgraphs/name/fuseio/fuseswap')
+export const fuseswapClient = new GraphQLClient('https://api.thegraph.com/subgraphs/name/fuseio/fuseswap')
 
-export const blockClient = new GraphQLClient('https://graph.fuse.io/subgraphs/name/fuseio/fuse-blocks')
+export const blockClient = new GraphQLClient('https://api.thegraph.com/subgraphs/name/fuseio/fuse-blocks')
 
-export const bridgeClient = new GraphQLClient('https://graph.fuse.io/subgraphs/name/fuseio/fuse-ethereum-bridge')
+export const bridgeClient = new GraphQLClient('https://api.thegraph.com/subgraphs/name/fuseio/fuse-ethereum-bridged-tokens')
