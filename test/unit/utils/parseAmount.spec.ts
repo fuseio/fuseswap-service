6e01afa4d96f7e8ff9d3d9a12ddac6d0bfabab9d
Lior Agnin <lior.agnin@gmail.com>
2021-11-28 16:37:40 +0200

    WIP

--- test/unit/utils/parseAmount.spec.ts
+++ test/unit/utils/parseAmount.spec.ts
@@ -13,6 +13,8 @@ describe('parseAmount', () => {
   })
 
   test('should handle values that contain a fractional component that exceeds decimals', () => {
+    // TODO - this test should work
+    // expect(parseAmount('55.034086813321221958', ETHER)?.raw.toString()).toBe('55034086813321221958')
     expect(parseAmount('9.1234567', USDC)?.raw.toString()).toBe('9123456')
     expect(parseAmount('9.1234547', USDC)?.raw.toString()).toBe('9123454')
     expect(parseAmount('9.12345555555555', USDC)?.raw.toString()).toBe('9123455')
