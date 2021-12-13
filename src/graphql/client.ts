e26be8828767c3c127ad309876d0ada58547e0d2
mul53 <mulengabowa53@gmail.com>
2021-12-13 12:21:45 +0200

    add memory fixes (#58)
    * add memory fixes
    
    * add lint fixes
    
    * fix start-prod scriptt

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,7 +1,16 @@
 import { GraphQLClient } from 'graphql-request'
+import config from 'config'
 
-export const fuseswapClient = new GraphQLClient('https://api.thegraph.com/subgraphs/name/fuseio/fuseswap')
+const timeout = parseInt(config.get('api.timeout'))
 
-export const blockClient = new GraphQLClient('https://api.thegraph.com/subgraphs/name/fuseio/fuse-blocks')
+export const fuseswapClient = new GraphQLClient('https://api.thegraph.com/subgraphs/name/fuseio/fuseswap', {
+  timeout
+})
 
-export const bridgeClient = new GraphQLClient('https://api.thegraph.com/subgraphs/name/fuseio/fuse-ethereum-bridged-tokens')
+export const blockClient = new GraphQLClient('https://api.thegraph.com/subgraphs/name/fuseio/fuse-blocks', {
+  timeout
+})
+
+export const bridgeClient = new GraphQLClient('https://api.thegraph.com/subgraphs/name/fuseio/fuse-ethereum-bridged-tokens', {
+  timeout
+})
