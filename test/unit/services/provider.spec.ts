2e1921d145a68854a83d2a6ece1993faee45b052
Lior Agnin <lior.agnin@gmail.com>
2022-11-07 10:34:44 +0200

    REMOVED rpc test

--- test/unit/services/provider.spec.ts
+++ test/unit/services/provider.spec.ts
@@ -6,13 +6,6 @@ describe('ProviderService', () => {
     expect(ProviderService.getProvider()).toBeInstanceOf(JsonRpcProvider)
   })
 
-  test('instance rpc url should be fuse network url', () => {
-    const {
-      connection: { url },
-    } = ProviderService.getProvider() as JsonRpcProvider
-    expect(url).toBe('https://rpc.fuse.io')
-  })
-
   test('creates only one instance', () => {
     expect(ProviderService.getProvider()).toBe(ProviderService.getProvider())
   })
